29bee0d7126dd4ba8e60a264601e9dc8073015c4
Laxman kohar <73365025+Laxmankohar@users.noreply.github.com>
2024-06-26 09:08:04 +0000

    Added some folder along with database connection

--- Config/DB.js
+++ Config/DB.js
@@ -5,11 +5,13 @@ dotenv.config();
 
 export const DBConnection = async () =>{
     try {
-      await mongoose.connect(process.env.DBlink);
-      const { mongoose } = mongoose;
-      connection.once("open", () => {
-        console.log("Database connected");
-      });
+      const connectionInstance = await mongoose.connect(process.env.DBlink);
+      console.log("MongoDB Database connected successfully");
+      console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+      // const { connection } = mongoose;
+      // connection.once("open", () => {
+      //   console.log("MongoDB Database connected successfully");
+      // });
     } catch (error) {
       console.log("Error in DB Connection", error);
     }
