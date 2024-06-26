b95021b3272884343d65eb8f3171204318508efa
Laxman kohar <73365025+Laxmankohar@users.noreply.github.com>
2024-06-26 10:12:01 +0000

    User Model is created

--- Config/DB.js
+++ Config/DB.js
@@ -3,16 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const DBConnection = async () =>{
-    try {
-      const connectionInstance = await mongoose.connect(process.env.DBlink);
-      console.log("MongoDB Database connected successfully");
-      console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-      // const { connection } = mongoose;
-      // connection.once("open", () => {
-      //   console.log("MongoDB Database connected successfully");
-      // });
-    } catch (error) {
-      console.log("Error in DB Connection", error);
-    }
+export const DBConnection = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDB Database connected successfully");
+    console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log("Error in DB Connection", error);
+  }
 } 
